b2f79fcdc73eb3fba39921c8ab7f9c5fd50cc4c0
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2022-04-24 20:49:25 -0400

    controles, rutas y vistas usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :usuarios
+  devise_for :usuarios, :controllers => {
+    sessions: 'usuarios/sessions',
+    registrations: 'usuarios/registrations'
+  },
+  :path => '', :path_names => {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register',
+    edit: 'perfil'
+  }
+
   root 'home#index'
 end
