27d5488901bcfa30972772abd27636a7c0a219d3
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2022-05-01 18:58:24 -0400

    ver, editar, eliminar turnos

--- app/controllers/turnos_controller.rb
+++ app/controllers/turnos_controller.rb
@@ -4,10 +4,6 @@ class TurnosController < ApplicationController
         @turnos = Turno.all
     end
 
-    def new
-        @test = "asad"
-    end
-
     def create
         @turno = Turno.new(
             dia: params[:dia],
@@ -29,4 +25,33 @@ class TurnosController < ApplicationController
     def turno
         @turno = Turno.find(params[:id])
     end
+
+    def delete
+        Turno.find(params[:id]).destroy
+        redirect_to root_path
+    end
+
+    def edit
+        @turno = Turno.find(params[:id])
+    end
+
+    def update
+        Turno.update(
+            params[:id],
+            dia: turno_params[:dia],
+            direccion_salida: turno_params[:direccion_salida],
+            hora_salida: turno_params[:hora_salida],
+            tipo: turno_params[:tipo],
+            cupos: turno_params[:cupos],
+            campus: turno_params[:campus]
+        )
+
+        redirect_to Turno.find(params[:id])
+    end
+
+    private
+        def turno_params
+            params.require(:turno)
+        end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'crear_turno', to: 'turnos#new', as: :new_turno
   post 'crear_turno', to: 'turnos#create', as: :turnos
   get 'turno/:id', to: 'turnos#turno', as: :turno
+  delete 'turno/:id', to: 'turnos#delete'
+  patch 'turno/:id', to: 'turnos#update', as: :patch_turno
+  get 'turno/editar/:id', to: 'turnos#edit', as: :edit_turno
 end
