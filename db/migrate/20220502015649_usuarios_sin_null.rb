a9d52d608bac8878f91064c5ad6e9cbdd4db07f4
taacc414 <mdarto@protonmail.com>
2022-05-02 13:05:45 -0400

    agregar html

---
+++ db/migrate/20220502015649_usuarios_sin_null.rb
@@ -0,0 +1,8 @@
+class UsuariosSinNull < ActiveRecord::Migration[6.0]
+  def change
+    #change_column_null :usuarios, :nombre, false
+    #change_column_null :usuarios, :imagen_perfil, false
+    #change_column_null :usuarios, :direccion, false
+    #change_column_null :usuarios, :telefono, false
+  end
+end
