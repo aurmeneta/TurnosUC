9bc14f04fecb19373b380b6f17f4dc1b5a0a6200
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2022-05-15 22:15:30 -0400

    chat: enviar y ver mensajes

--- app/controllers/mensajes_controller.rb
+++ app/controllers/mensajes_controller.rb
@@ -1,2 +1,15 @@
 class MensajesController < ApplicationController
+    def create
+        @mensaje = Mensaje.new(
+            turno_id: params[:turno_id],
+            usuario_id: current_usuario.id,
+            contenido: params[:mensaje]
+        )
+
+        if @mensaje.save
+            redirect_to turno_path(params[:turno_id])
+        else
+            redirect_to turno_path(params[:turno_id]), alert: "No se pudo envíar el mensaje"
+        end
+    end
 end
--- app/models/turno.rb
+++ app/models/turno.rb
@@ -5,10 +5,24 @@ class Turno < ApplicationRecord
   has_many :solicituds
   has_many :mensajes
 
-  validates :usuario, :direccion_salida, presence: true
+  validates :usuario_id, :direccion_salida, presence: true
   validates :dia, inclusion: { in: %w[Lunes Martes Miércoles Jueves Viernes Sabádo] }
   validates :hora_salida, format: { with: /[0-9]{1,2}:[0-9]{2}/ }
   validates :tipo, inclusion: { in: %w[Ida Vuelta] }
   validates :cupos, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :campus, inclusion: { in: ['San Joaquín', 'Casa Central', 'Oriente', 'Lo Contador', 'Villarrica'] }
+
+
+  def usuario_en_turno(id_usuario)
+    if self.usuario.id == id_usuario
+      return true
+    else
+      self.solicituds.each do |solicitud|
+        if solicitud.descripcion == "Aceptada" && solicitud.usuario.id == id_usuario
+          return true
+        end
+      end
+      return false  
+    end
+  end
 end
