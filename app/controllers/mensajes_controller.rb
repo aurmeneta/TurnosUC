33b734fef48656ea370fce7094540ec5458fae2e
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2022-05-17 19:49:35 -0400

    rubocop y crear mensajes solo miembros

--- app/controllers/mensajes_controller.rb
+++ app/controllers/mensajes_controller.rb
@@ -1,15 +1,21 @@
-class MensajesController < ApplicationController
-    def create
-        @mensaje = Mensaje.new(
-            turno_id: params[:turno_id],
-            usuario_id: current_usuario.id,
-            contenido: params[:mensaje]
-        )
+# frozen_string_literal: true
 
-        if @mensaje.save
-            redirect_to turno_path(params[:turno_id])
-        else
-            redirect_to turno_path(params[:turno_id]), alert: "No se pudo envíar el mensaje"
-        end
+class MensajesController < ApplicationController
+  def create
+    if Turno.find(params[:turno_id]).usuario_en_turno(current_usuario.id)
+      @mensaje = Mensaje.new(
+        turno_id: params[:turno_id],
+        usuario_id: current_usuario.id,
+        contenido: params[:mensaje]
+      )
+
+      if @mensaje.save
+        redirect_to turno_path(params[:turno_id])
+      else
+        redirect_to turno_path(params[:turno_id]), alert: 'No se pudo envíar el mensaje'
+      end
+    else
+      redirect_to turno_path(params[:turno_id]), alert: 'Debes estar en un turno para envíar un mensaje en su chat', status: :forbidden
     end
+  end
 end
