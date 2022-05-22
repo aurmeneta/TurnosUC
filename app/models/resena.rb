fe93dd6af56abb1b08bcd2087e18a8434422ff92
Andrés Urmeneta Betzhold <aurmenetab@gmail.com>
2022-05-21 21:56:59 -0400

    resenas test

--- app/models/resena.rb
+++ app/models/resena.rb
@@ -4,5 +4,7 @@ class Resena < ApplicationRecord
   belongs_to :usuario
   belongs_to :autor, class_name: "Usuario"
 
-  validates :usuario_id, :autor_id, :calificacion, presence: true
+  validates :usuario_id, :autor_id, :calificacion, :contenido, presence: true
+  validates :calificacion, numericality: { only_integer: true, 
+    greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
